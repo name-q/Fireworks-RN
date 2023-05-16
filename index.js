83cad543cfeba2ded9fa85680f8bc32da264213c
name-q <1620206666@qq.com>
2023-05-16 14:03:35 +0800

    +@react-navigation/devtools 不影响生产 ⚠️TODO: Redux DevTools Extension

--- index.js
+++ index.js
@@ -1,17 +1,10 @@
 /**
  * @format
  */
-
+import * as React from 'react';
 import { AppRegistry } from 'react-native';
 import App from '@/pages/App';
 import { name as appName } from './app.json';
 
-import { NavigationContainer } from '@react-navigation/native';
 
-AppRegistry.registerComponent(appName, () => {
-    return (
-        <NavigationContainer>
-            <App />
-        </NavigationContainer>
-    )
-});
+AppRegistry.registerComponent(appName, () => App);
